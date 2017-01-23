83d5dda983c2f5fe73c6e83cb3b12136750095d1
origin_zy <enginezy@gmail.com>
2017-01-23 22:24:41 +0800

    v3.0

--- src/main/java/com/hudongwx/demo/IndexController.java
+++ src/main/java/com/hudongwx/demo/IndexController.java
@@ -21,18 +21,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-    @RequestMapping("/index1")
+    @RequestMapping("/")
     public String index0(){
+        return "我是spring boot的测试首页 V4.0";
+    }
+
+    @RequestMapping("/index1")
+    public String index1(){
         return "我是spring boot的测试首页 V3.1";
     }
 
     @RequestMapping("/index2")
-    public String index1(){
+    public String index2(){
         return "测demo成功了";
     }
 
     @RequestMapping("/index3")
-    public String index2(){
+    public String index3(){
         return "测demo成功了";
     }
 
